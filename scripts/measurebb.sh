afbfcb02a6513f2475276f90622cef511532eeef
klaspihl <klas.pihl@gmail.com>
2026-09-10 10:14:47 +0200

    Use client 5.1.0 native json output

--- scripts/measurebb.sh
+++ scripts/measurebb.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-FILE="/app/bbk.log"
-/usr/bin/timeout 120 /app/bbk --quiet > $FILE
-	TIMESTAMP=$(date '+%s')
-	UPLOAD=$(awk '{print $3}' $FILE)
-	PING=$(awk '{print $1}' $FILE)
-	DOWNLOAD=$(awk '{print $2}' $FILE)
-  echo -e "{\n    \"Download\": $DOWNLOAD,\n    \"Upload\": $UPLOAD,\n    \"Ping\": $PING\n}"
+/usr/bin/timeout 120 /app/bbk --json
